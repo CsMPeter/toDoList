da05389fd1ceeedd868ce1cba51d5ee6d97425d0
CsMPeter <csurkampeter@gmail.com>
2019-10-21 20:05:01 +0300

    added update delete and get todoitems

---
+++ src/main/java/org/fasttrackit/todolist/App.java
@@ -0,0 +1,30 @@
+package org.fasttrackit.todolist;
+
+import org.fasttrackit.todolist.domain.TodoItem;
+import org.fasttrackit.todolist.persistance.ToDoItemRepository;
+import org.fasttrackit.todolist.transfer.CreateToDoItemRequest;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws SQLException, IOException, ClassNotFoundException {
+
+        CreateToDoItemRequest request = new CreateToDoItemRequest();
+        request.setDescription("Learn JDBC");
+        request.setDeadline(LocalDate.now().plusWeeks(1));
+        ToDoItemRepository toDoItemRepository = new ToDoItemRepository();
+//        toDoItemRepository.createToDoItem(request);
+//        toDoItemRepository.updateToDoItem(2, false);
+//        toDoItemRepository.deleteToDoItem(9);
+        List<TodoItem> toDoItems = toDoItemRepository.getToDoItems();
+        System.out.println(toDoItems);
+    }
+}
---
+++ src/main/java/org/fasttrackit/todolist/persistance/ToDoItemRepository.java
@@ -0,0 +1,76 @@
+package org.fasttrackit.todolist.persistance;
+
+import org.fasttrackit.todolist.domain.TodoItem;
+import org.fasttrackit.todolist.transfer.CreateToDoItemRequest;
+
+import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ToDoItemRepository {
+
+    public void createToDoItem(CreateToDoItemRequest request) throws SQLException, IOException, ClassNotFoundException {
+        String sql = "INSERT INTO to_do_item (description, deadline) VALUES(? ,?)";
+        //try with resources
+        try (Connection connection = DatabaseConfiguration.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)
+        ) {
+            preparedStatement.setString(1, request.getDescription());
+            preparedStatement.setDate(2, Date.valueOf(request.getDeadline()));
+
+            preparedStatement.executeUpdate();
+        }
+
+    }
+
+    public void updtaeToDoItem(long id, boolean done) throws SQLException, IOException, ClassNotFoundException {
+        String sql = "UPDATE to_do_item Set done=? Where id=?";
+
+        try (Connection connection = DatabaseConfiguration.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+            preparedStatement.setBoolean(1, done);
+            preparedStatement.setLong(2, id);
+
+            preparedStatement.executeUpdate();
+
+        }
+
+    }
+
+    public void deleteToDoItem(long id) throws SQLException, IOException, ClassNotFoundException {
+        String sql = "DELETE FROM to_do_item where id=?";
+
+        try (Connection connection = DatabaseConfiguration.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+            preparedStatement.setLong(1,id);
+
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    public List<TodoItem> getToDoItems() throws SQLException, IOException, ClassNotFoundException {
+        String sql = "SELECT id, description, deadline, done FROM to_do_item";
+
+        List<TodoItem> todoItems = new ArrayList<>();
+
+        try(Connection connection = DatabaseConfiguration.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultset = statement.executeQuery(sql)){
+
+            while(resultset.next()){
+                TodoItem todoItem = new TodoItem();
+                todoItem.setId(resultset.getLong("id"));
+                todoItem.setDeadline(resultset.getDate("deadline").toLocalDate());
+                todoItem.setDescription(resultset.getString("description"));
+                todoItem.setDone(resultset.getBoolean("done"));
+
+                todoItems.add(todoItem);
+            }
+
+        }
+
+        return todoItems;
+    }
+
+}
